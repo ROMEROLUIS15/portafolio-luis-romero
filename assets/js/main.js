2a11e359493c4e713a297bdd5f0c89a3d9db6a8a
Luis Romero <lueduar15@gmail.com>
2024-02-26 14:55:33 -0400

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Backend Developer","Desarrollador Backend"],
+  strings : ["a Backend Developer","un Desarrollador Backend"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
